27519e1b89b241265fe0183716a2cf52038cea4f
Dmitry Popov <dmitry@parsec-club.ru>
2017-07-30 03:33:34 +0300

    Final one?

--- testpublishreport.ps1
+++ testpublishreport.ps1
@@ -4,11 +4,8 @@ $openCoverConsole = $ENV:USERPROFILE + '\.nuget\packages\OpenCover\4.6.519\tools
 $codecovUploader = $ENV:USERPROFILE + '\.nuget\packages\codecov\1.0.1\tools\codecov.exe'
 $target = '-target: C:\Program Files\dotnet\dotnet.exe'
 
-# Rebuild with full debug info
-& dotnet build /p:DebugType=full
-
 # Configuration and execution of the tests
-$targetArgs = '-targetargs: test test/RecurrentTasks.Tests/RecurrentTasks.Tests.csproj -f netcoreapp1.0 /p:DebugType=full'
+$targetArgs = '-targetargs: test test/RecurrentTasks.Tests/RecurrentTasks.Tests.csproj'
 $filter = '-filter: +[RecurrentTasks*]* -[RecurrentTasks.Tests*]*'
 $output = '-output:' + $resultsFile
 & $openCoverConsole $target $targetArgs '-register:user' $filter $output '-oldStyle'
