55b8a5dc4c2c8f3d86952a85711625cbb6a5eb7f
Dmitry Popov <dmitry@parsec-club.ru>
2024-03-05 11:02:42 +0300

    fix: codecov results

--- testpublishreport.ps1
+++ testpublishreport.ps1
@@ -1,14 +1,4 @@
-# General configurations, such as location of the code coverage report, location of the OpenCover and location of .NET
-$resultsFile = 'opencover-result.xml'
-$openCoverConsole = $ENV:USERPROFILE + '\.nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe'
-$codecovUploader = $ENV:USERPROFILE + '\.nuget\packages\codecov\1.9.0\tools\codecov.exe'
-$target = '-target: C:\Program Files\dotnet\dotnet.exe'
+$codecovUploader = $ENV:USERPROFILE + '\.nuget\packages\codecovuploader\0.7.2\tools\codecov.exe'
 
-# Configuration and execution of the tests
-$targetArgs = '-targetargs: test test/RecurrentTasks.Tests/RecurrentTasks.Tests.csproj'
-$filter = '-filter: +[RecurrentTasks*]* -[RecurrentTasks.Tests*]*'
-$output = '-output:' + $resultsFile
-& $openCoverConsole $target $targetArgs '-register:user' $filter $output '-oldStyle'
-
-# Upload to codecov.io
-& $codecovUploader -f $resultsFile -t 554bb7e1-09d2-4f3a-9b72-c2818e370f94
+& dotnet test /p:AltCover=true
+& $codecovUploader -t 554bb7e1-09d2-4f3a-9b72-c2818e370f94
